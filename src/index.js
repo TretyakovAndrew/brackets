44e80a84ad4a791e261a4bc84aa8e5fbcbeb2dd7
Andrew Tretyakov <drobservis@gmail.com>
2022-07-13 00:11:02 +0300

    feat: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    for (let elem of bracketsConfig) {
+      if (str[i] === elem[0] && str[i + 1] === elem[1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length);
+        i -= 2;
+      }
+    }
+  }
+  return str.length === 0;
 }
